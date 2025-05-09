4c5987f87f9ee5cc071f57293a56636a3463696c
Ana Flavia <anafpoliveira3@gmail.com>
2025-05-09 20:12:56 +0100

    Adiciona o cabeçalho correto

--- app.py
+++ app.py
@@ -6,8 +6,15 @@ car_data = pd.read_csv('vehicles.csv')
 hist_button = st.button('Criar histograma') # criar um botão
 scatter_button = st.button('Criar um gráfico de dispersão') #criar um botão
 
-st.header('Anúncio de vendas de carro')
-        
+st.header('Relação entre Preço e Quilometragem Percorrida')
+st.markdown(
+    "<p style='font-size: 14px;'>"
+    "Este gráfico de dispersão mostra como o preço dos veículos varia de acordo com a quilometragem percorrida (hodômetro). "
+    "Em geral, espera-se que veículos com maior quilometragem tenham preços mais baixos, mas também é possível identificar exceções "
+    "ou padrões por ano, tipo de combustível e outros fatores."
+    "</p>",
+    unsafe_allow_html=True)
+
 if hist_button: 
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram(car_data, x="odometer") # criar um histograma
